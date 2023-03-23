685b0e5f3474111c9e1f5f4cec3e5c3a0725a2de
Александр Белоусов <aleksander.belousovv@gmail.com>
2023-03-23 22:28:25 +0300

    init-main-page

---
+++ shelter/shelter.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper__button_next',
+      prevEl: '.swiper__button_prev',
+    },
+  
+    
+  });
+  
